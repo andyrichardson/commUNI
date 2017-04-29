b82c379646ead1f7727b53e121f079eea877d45f
Jake Lankester <Jake Lankester>
2017-04-29 16:49:00 +0100

    correct account details m8

--- model/Token.js
+++ model/Token.js
@@ -1,5 +1,5 @@
 var db = require('seraph')({
-  server: "http://localhost:7474",
+  server: "http://192.168.99.100:7474",
   user: "neo4j",
   pass: "pass"
 });
@@ -12,3 +12,20 @@ module.exports.register = function(first, last, email){
     }
   });
 }
+
+module.exports.login = function(email, password){
+  var query = `MATCH (n:User {email: "${email}", password: "${password}"}) return n`
+  db.query(query, function(err, done){
+    if(err){
+      console.log(err);
+    }
+    if(done[0] !== undefined){
+      console.log('we good');
+    }
+    else{
+      console.log('nahh mate');
+    }
+  });
+}
+
+module.exports.login("abc@abc.com","pass")
