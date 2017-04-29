84e4293b87514f92096ec1652331e46ca127f173
Andy Richardson <contact@andythedeveloper.com>
2017-04-30 00:29:39 +0100

    login JS

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,3 +1,26 @@
-$("#login-submit").click(function(event) {
-  console.log('hello');
+$(document).ready(function() {
+  $("#login-submit").on('click', function(event) {
+    event.preventDefault(event);
+    const password = $("#password").val();
+    const username = $("#username").val();
+    $.ajax({
+      url: '/api/login',
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        email: username,
+        password: password
+      }
+    })
+    .done(function(data) {
+      if(data.token === undefined){
+        return alert("unable to log in");
+      }
+
+      document.cookie = "token=" + data.token;
+      window.location.href = "/uniofessex";
+    });
+  });
 });
+
+console.log('here');
