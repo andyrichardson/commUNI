c6f5b106ab93fef3913795223891ec6212aca2cb
James <lowry2234@gmail.com>
2017-04-29 17:56:20 +0100

    complete registration and login

--- routes/api.js
+++ routes/api.js
@@ -2,18 +2,32 @@
 
 //get = get information, post = put info into db, patch = change info in db
 var express = require('express');
+var token = require('../model/Token.js');
 var router = express.Router();
 
 router.post('/register', function(req, res, next){
-  res.json({message: "You're here to register"});
+  console.log(req.body);
+  //pass the token the different variables to use with register function
+  //check Token.js for more functions as written to implement
+  token.register(req.body.firstName, req.body.lastName, req.body.email, req.body.password, function(err){
+    if(err){
+      return res.json({message: "unable to register"});
+    }
+    res.json({message: "registration successful"});
+  });
 });
 
 router.get('/', function(req, res, next){
   res.json({user: "joe", password:"test"});
 });
 
-router.get('/login', function(req, res, next) {
-  res.json({token: "I'm a token"});
+router.post('/login', function(req, res, next) {
+  token.login(req.body.email, req.body.password, function(err, token){
+    if(err){
+      return res.json({message: "Unable to log you in."});
+    }
+    res.json({token: token, message: "login successful"});
+  });
 });
 
 router.post('/newgroup', function(req, res, next){
